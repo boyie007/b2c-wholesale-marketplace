b0701aaa24632ce641763708244bf5080ace513c
boyie007 <160201160+boyie007@users.noreply.github.com>
2026-01-15 08:37:07 +0100

    Fixed duplicate PORT declaration

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
-const PORT = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
